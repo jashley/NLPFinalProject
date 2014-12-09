9492be5f6ec1ebf21ea8152e8bef12758f2360b1
KSchaefer13 <kathleen_schaefer@hmc.edu>
2014-12-08 18:58:05 -0800

    working on scorer

--- sentence_scorer.py
+++ sentence_scorer.py
@@ -1,5 +1,8 @@
 # Kathleen Schaefer and Johnathan Ashley
 
+# ideas from
+# http://courses.ischool.berkeley.edu/i256/f06/papers/edmonson69.pdf
+
 #pip install -U numpy scipy scikit-learn
 from sklearn.feature_extraction.text import TfidfVectorizer #
 import numpy as np
@@ -151,6 +154,69 @@ def getRandomScores(doc):
 		scores += [random.random()]
 	return scores
 
+def countWords(corpus):
+	""" counts occurences of words in a corpus """
+	docCounts = {}
+	totalCounts = {}
+	for doc in corpus:
+		docDic = {}
+		for sentence in doc:
+			splitted = sentence.split('\n')
+			for line in splitted:
+				s = line.split()
+				for word in s:
+					docDic[word] = 1.0
+					if word in totalCounts:
+						totalCounts[word] += 1.0
+					else:
+						totalCounts[word] = 1.0
+		for word in docDic:
+			if word in docCounts:
+				docCounts[word] += 1.0
+			else:
+				docCounts[word] = 1.0
+	return docCounts, totalCounts
+
+
+def scoreWordsInDoc(numDocs, totalCounts, docCounts, totalDoc):
+	""" scores words based on how likely they are to 
+	be key words """
+	scores = {}
+	numWords = len(totalDoc)
+	for word in totalDoc:
+		freq = totalDoc[word]
+		if word in docCounts:
+			totalVal = totalCounts[word]
+			docVal = docCounts[word]
+			if docVal/numDocs >= .6:
+				print word, "too many docs"
+				scores[word] = 0.0
+			elif totalVal/docVal > freq:
+				print word, "too infrequent"
+				scores[word] = 0.0
+			#elif freq/numWords < .001:
+				#print word, "uncommon word"
+				#scores[word] = 0.0
+			elif getAlphaRatio(word) < .75:
+				print word, "non-alpha"
+				scores[word] = 0.0
+			else:
+				score = freq/docVal
+				scores[word] = score
+				print word, score
+	return scores
+
+def getAlphaRatio(word):
+	length = len(word)
+	alpha = 0.0
+	for letter in word:
+		if letter.isalpha():
+			alpha += 1.0
+	#print "ALPHA", word, alpha/length
+	return alpha/length
+
+
+
 
 if __name__ == '__main__':
 	inputFile = sys.argv[1]
@@ -159,22 +225,29 @@ if __name__ == '__main__':
 	randomScores = getRandomScores(document)
 
 	corpus = []
+	numDocs = 0.0
 	for filename in os.listdir('corpus/fulltext'):
 		if ('.xml' in filename):
+			numDocs += 1.0
 			corpus += [st.writeSentences(filename)]
 
-	corp_tf_idf = trainTFID(corpus)
-	example_tf_idf = trainTFID([document])
-	keywords = getKeyWords(corp_tf_idf, example_tf_idf)
-	#print corp_tf_idf
-	tfidf_ranks = tfidfRank(corp_tf_idf, document)
-	keywords_scores = scoreByKeyWords(keywords, document)
+	totalCorpus, docCorpus = countWords(corpus)
+	totalDoc, x = countWords([document])
+	scoreWords = scoreWordsInDoc(numDocs, totalCorpus, docCorpus, totalDoc)
 
-	print keywords_scores, tfidf_ranks
+	#corp_tf_idf = trainTFID(corpus)
+	#example_tf_idf = trainTFID([document])
+	#keywords = getKeyWords(corp_tf_idf, example_tf_idf)
+	#print corp_tf_idf
+	#tfidf_ranks = tfidfRank(corp_tf_idf, document)
+	#keywords_scores = scoreByKeyWords(keywords, document)
+	keywords_scores = scoreByKeyWords(scoreWords, document)
+
+	#print keywords_scores, tfidf_ranks
 
 	print "ORIGINAL DOCUMENT"
 	print
-	printDoc(document)
+	#printDoc(document)
 	print
 	print "RANDOM SUMMARY"
 	print
@@ -186,5 +259,5 @@ if __name__ == '__main__':
 	print
 	print "TFIDF SUMMARY"
 	print
-	print getSummary(tfidf_ranks, document, 2)
+	#print getSummary(tfidf_ranks, document, 2)
 
